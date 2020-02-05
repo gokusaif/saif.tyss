dc07b805dd7a043c171ca3efd27d3709019e37d5
gokusaif <ota.sa.97@gmail.com>
2020-02-05 15:51:19 +0530

    update

--- surveyapp/src/main/java/com/tyss/surveyapp/exceptions/AdminException.java
+++ surveyapp/src/main/java/com/tyss/surveyapp/exceptions/AdminException.java
@@ -2,6 +2,11 @@ package com.tyss.surveyapp.exceptions;
 
 public class AdminException extends RuntimeException{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public AdminException(String message) {
 		super(message);
 	}
