0b41c46aa4f78c137de7172573f82943c7f7463f
omkarnaik <omkarnaik @gmail.com>
2020-02-05 14:39:26 +0530

    wqe3we
    Signed-off-by: omkarnaik  <omkarnaik @gmail.com>

---
+++ surveyapp/src/main/java/com/tyss/surveyapp/dto/QuestionDetailsDto.java
@@ -0,0 +1,18 @@
+package com.tyss.surveyapp.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table
+@Data
+public class QuestionDetailsDto {
+	
+	
+	
+	
+	
+
+}
